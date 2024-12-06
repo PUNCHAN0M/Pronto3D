d8b5f5f39181c211df8786ff9342e888e50a6768
PUNCHAN0M <130173517+PUNCHAN0M@users.noreply.github.com>
2024-12-06 11:52:16 +0700

    add camera

--- Pronto3D/src/App.tsx
+++ Pronto3D/src/App.tsx
@@ -1,16 +1,16 @@
 import './App.css'
 import { Routes ,BrowserRouter, Route } from 'react-router-dom'
 import Home from './Home'
+import WebcamComponent from './ScanQRCodePage'
 function App() {
 
   return (
-<BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home/>}></Route>
-    </Routes>
-</BrowserRouter>
-
-  )
-}
+  <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/camera" element={<WebcamComponent/>}></Route>
+      </Routes>
+  </BrowserRouter>
+  )}
 
 export default App
